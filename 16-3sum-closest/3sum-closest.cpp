f51bd6e9c1476eb601efbe0170d479607a8a279a
MD FAIZAN HUSSAIN <124052736+Mdfaizanhussain@users.noreply.github.com>
2025-12-19 05:15:40 +0530

    Time: 19 ms (17.68%) | Memory: 14.1 MB (26.88%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -1,25 +1,46 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int n=nums.size();
-        int bestsum=0;
-        int bestdiff=INT_MAX;
+        // int n=nums.size();
+        // int bestsum=0;
+        // int bestdiff=INT_MAX;
        
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         for(int k=j+1;k<n;k++){
 
-                    int sum=nums[i]+nums[j]+nums[k];
-                    int diff=abs(sum-target);
-                    if(diff<bestdiff){
-                        bestdiff=diff;
-                        bestsum=sum;
-                    }
-                }
-            }
-        }
-        return bestsum;
-    }
-};
+        //             int sum=nums[i]+nums[j]+nums[k];
+        //             int diff=abs(sum-target);
+        //             if(diff<bestdiff){
+        //                 bestdiff=diff;
+        //                 bestsum=sum;
+        //             }
+        //         }
+        //     }
+        // }
+        // return bestsum;
+
 //if index not asked then sort it
 //apply 2 pointer
+          int n=nums.size();
+        int bestsum=0;
+        int bestdiff=INT_MAX;
+sort(nums.begin(),nums.end());
+    for(int k=0;k<=n-3;k++){
+        int i=k+1;
+        int j=n-1;
+        while(i<j){
+            int sum=nums[i]+nums[j]+nums[k];
+            int diff=abs(sum-target);
+            if(diff<bestdiff){
+                bestdiff=diff;
+                bestsum=sum;
+            }
+            if(sum<target)i++;
+            else j--;
+        }
+    }
+    return bestsum;
+//time com is o(n*n)
+    }
+};
